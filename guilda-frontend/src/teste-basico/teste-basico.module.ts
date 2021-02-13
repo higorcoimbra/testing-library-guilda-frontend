2982a62851656d9393fb61f5d65f0615eafbfac3
Higor Coimbra <higor.coimbra@dtidigital.com.br>
2021-02-12 22:13:34 -0300

    including a new component to demonstrate a basic test as is

---
+++ guilda-frontend/src/teste-basico/teste-basico.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TesteBasicoComponent } from './teste-basico/teste-basico.component';
+
+
+
+@NgModule({
+  declarations: [TesteBasicoComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TesteBasicoModule { }
---
+++ guilda-frontend/src/teste-basico/teste-basico/teste-basico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teste-basico',
+  templateUrl: './teste-basico.component.html',
+  styleUrls: ['./teste-basico.component.css']
+})
+export class TesteBasicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
