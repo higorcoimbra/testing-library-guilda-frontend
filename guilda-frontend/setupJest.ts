baed200075f6550c94b4bc8a74b1ec72399e6209
Higor Coimbra <higor.coimbra@dtidigital.com.br>
2021-02-16 17:19:59 -0300

    includes configuration to prevent getByRole and Material errors

--- guilda-frontend/setupJest.ts
+++ guilda-frontend/setupJest.ts
@@ -1,1 +1,14 @@
 import '@testing-library/jest-dom';
+import { configure } from '@testing-library/dom';
+
+configure({
+    defaultHidden: true
+});
+
+Object.defineProperty(window, 'getComputedStyle', {
+    value: () => ({
+        getPropertyValue: (prop) => {
+            return '';
+        }
+    })
+});
