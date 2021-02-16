8d42824f673e2ab23814b3f7f00cba9dc8bc33b0
Higor Coimbra <higor.coimbra@dtidigital.com.br>
2021-02-15 23:07:06 -0300

    configuration files and updates for angular jest builder and test setup

---
+++ guilda-frontend/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    moduleNameMapper: {
+        '^src/(.*)$': '<rootDir>/src/$1',
+        '^app/(.*)$': '<rootDir>/src/app/$1',
+        '^assets/(.*)$': '<rootDir>/src/assets/$1',
+        '^(.*)/environments/(.*)$': '<rootDir>/src/environments/environment.jest.ts',
+    },
+    collectCoverageFrom: [
+        "src/app/**/*.ts",
+        "!src/app/**/*.module.ts",
+        "!src/app/**/*.array.ts",
+        "!src/app/**/*.model.ts",
+        "!src/app/fragmentTypes.ts",
+    ],
+    modulePaths: ['<rootDir>/dist'],
+    setupFilesAfterEnv: [
+        "./node_modules/@angular-builders/jest/dist/jest-config/setup.js",
+        "./setupJest.js"
+    ]
+};
+  
